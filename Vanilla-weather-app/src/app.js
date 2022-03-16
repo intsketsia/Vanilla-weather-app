efbccefdc21f043bdfbcebb59f293c3cb5e60723
Emmanuella Intsinzi <emmanuellaintsinzi@Emmanuellas-MacBook-Air.local>
2022-03-16 18:02:24 -0400

    showing the current temperature

--- Vanilla-weather-app/src/app.js
+++ Vanilla-weather-app/src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "2988cc0b462e792256914a6d30c3d866";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
